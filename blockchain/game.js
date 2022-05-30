167ed4c0b5c4d9c86e02770a6879f25575d6a6d1
Samihan <ssmuppirala@gmail.com>
2022-05-30 21:51:08 +0000

    moved everything to root

--- blockchain/game.js
+++ blockchain/game.js
@@ -1,8 +1,8 @@
 const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint256","name":"tokenCount","type":"uint256"}],"name":"addTokens","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getMyInfo","outputs":[{"internalType":"bool","name":"","type":"bool"},{"internalType":"uint256","name":"","type":"uint256"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getRoundNumber","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"initializePlayer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"playRound","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]
-const address = "0x5fe6B2d25D980514555cB3075A59E192e9499798"
+const address = "0x686964C19230d168f2dA74153a8280D64437a5b3"
 
 // Localhost address: "0x44f571951a721BEdDB3AE62A81C26A77b21B5757"
-// Replit testnet address: "0x5fe6B2d25D980514555cB3075A59E192e9499798"
+// Replit testnet address: "0x686964C19230d168f2dA74153a8280D64437a5b3"
 
 const gameContract = web3 => {
     return new web3.eth.Contract(abi, address)
